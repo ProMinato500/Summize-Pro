a09a6b97991964f3a3a6ab835d9b66aace6725c9
ProMinato500 <nandip500@gmail.com>
2023-05-22 11:10:57 +0530

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,10 @@ const Hero = () => {
         <button
           type="button"
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open(
+              "https://github.com/ProMinato500/article-summarizer",
+              "_blank"
+            )
           }
           className="black_btn"
         >
@@ -19,11 +22,12 @@ const Hero = () => {
 
       <h1 className="head_text">
         Summarize Articles with <br className="max-md:hidden" />
-        <span className="orange_gradient ">OpenAI GPT-4</span>
+        <span className="orange_gradient ">OpenAI GPT-4 Pro</span>
       </h1>
       <h2 className="desc">
-        Simplify your reading with Summize, an open-source article summarizer
-        that transforms lengthy articles into clear and concise summaries
+        Simplify your reading with Summize Pro, an open-source article
+        summarizer that transforms lengthy articles into clear and concise
+        summaries
       </h2>
     </header>
   );
